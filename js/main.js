408a610ee21eb8ea6df96f68b4caa979b760ca4f
RKalensky <roma.kalensky@gmail.com>
2016-08-06 23:32:02 +0300

    add(XMLHTTPRequest): getDataDb method

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-var currentDate = new Pikaday({
-    field: document.getElementById('currentDate'),
-    format: 'DD-MM-YYYY'
-});
+;(function(){
+    
+    var json = null,
+        xhr = new XMLHttpRequest();
+            
+    var filterDate = new Pikaday({
+        field: document.getElementById('filterDate'),
+        format: 'DD-MM-YYYY'
+    });
 
-var dueDate = new Pikaday({
-    field: document.getElementById('dueDate'),
-    format: 'DD-MM-YYYY'
-});
+    var dueDate = new Pikaday({
+        field: document.getElementById('dueDate'),
+        format: 'DD-MM-YYYY'
+    });
 
-var createdDate = new Pikaday({
-    field: document.getElementById('createdDate'),
-    format: 'DD-MM-YYYY'
-});
+    var createdDate = new Pikaday({
+        field: document.getElementById('createdDate'),
+        format: 'DD-MM-YYYY'
+    });
+    
+    function getDataDb(method, src, async) {
+        debugger;
+        xhr.open(method, src, async);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if(this.readyState != 4) return;
+            if(this.status != 200) {
+                alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+                return;
+            }
+            json = JSON.parse(xhr.responseText);
+        }
+    };
+    
+    getDataDb("GET", "/src/JSONData.json", true);
+    
+})();
